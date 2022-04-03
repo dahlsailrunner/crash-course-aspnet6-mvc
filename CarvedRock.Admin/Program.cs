5df29b27835df1114dea5d5e31b5b72d0e851c3e
Erik Dahl <erik.dahl@realpage.com>
2022-04-03 10:07:25 -0500

    removed empty line

--- CarvedRock.Admin/Program.cs
+++ CarvedRock.Admin/Program.cs
@@ -11,7 +11,6 @@ using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("CarvedRockAdminContextConnection");
 
-
 // Add services to the container.
 builder.Services.AddDbContext<ProductContext>();
 builder.Services.AddScoped<ICarvedRockRepository, CarvedRockRepository>();
