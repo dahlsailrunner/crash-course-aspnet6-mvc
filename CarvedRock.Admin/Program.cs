564ca70406ba0ecd08b0442ebdce225c709b1de8
Erik Dahl <erik.dahl@realpage.com>
2022-04-03 14:06:44 -0500

    fixed order of razor pages middleware

--- CarvedRock.Admin/Program.cs
+++ CarvedRock.Admin/Program.cs
@@ -76,9 +76,9 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
 
 app.Run();
