3d20792f69d92ee92c313cc09141d0d3023fd277
Erik Dahl <erik.dahl@realpage.com>
2023-11-23 06:22:43 -0600

    modified / updated for .net 8

--- CarvedRock.Admin/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ CarvedRock.Admin/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -2,51 +2,40 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using CarvedRock.Admin.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 
-namespace CarvedRock.Admin.Areas.Identity.Pages.Account
+namespace CarvedRock.Admin.Areas.Identity.Pages.Account;
+
+// Primary constructor and file-level namespace, along with removal of unused directives
+public class ConfirmEmailModel(UserManager<CarvedRockAdminUser> userManager) : PageModel
 {
-    public class ConfirmEmailModel : PageModel
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    [TempData]
+    public string StatusMessage { get; set; }
+    public async Task<IActionResult> OnGetAsync(string userId, string code)
     {
-        private readonly UserManager<CarvedRockAdminUser> _userManager;
-
-        public ConfirmEmailModel(UserManager<CarvedRockAdminUser> userManager)
+        if (userId == null || code == null)
         {
-            _userManager = userManager;
+            return RedirectToPage("/Index");
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [TempData]
-        public string StatusMessage { get; set; }
-        public async Task<IActionResult> OnGetAsync(string userId, string code)
+        var user = await userManager.FindByIdAsync(userId);
+        if (user == null)
         {
-            if (userId == null || code == null)
-            {
-                return RedirectToPage("/Index");
-            }
-
-            var user = await _userManager.FindByIdAsync(userId);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{userId}'.");
-            }
-
-            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
-            var result = await _userManager.ConfirmEmailAsync(user, code);
-            StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
-            return Page();
+            return NotFound($"Unable to load user with ID '{userId}'.");
         }
+
+        code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+        var result = await userManager.ConfirmEmailAsync(user, code);
+        StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+        return Page();
     }
 }
--- CarvedRock.Admin/Repository/CarvedRockRepository.cs
+++ CarvedRock.Admin/Repository/CarvedRockRepository.cs
@@ -3,33 +3,27 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CarvedRock.Admin.Repository;
 
-public class CarvedRockRepository : ICarvedRockRepository
+// modified to use primary constructor
+public class CarvedRockRepository(ProductContext context) : ICarvedRockRepository
 {
-    private readonly ProductContext _context;
-
-    public CarvedRockRepository(ProductContext context)
-    {
-        _context = context;
-    }
-
     public async Task<List<Product>> GetAllProductsAsync()
     {
-        return await _context.Products
+        return await context.Products
             .Include(p => p.Category)
             .ToListAsync();
     }
 
     public async Task<Product?> GetProductByIdAsync(int productId)
     {
-        return await _context.Products
+        return await context.Products
             .Include(p => p.Category)
             .FirstOrDefaultAsync(m => m.Id == productId);
     }
 
     public async Task<Product> AddProductAsync(Product product)
     {
-        _context.Products.Add(product);
-        await _context.SaveChangesAsync();
+        context.Products.Add(product);
+        await context.SaveChangesAsync();
         return product; // will have updated ID value
     }
 
@@ -37,12 +31,12 @@ public class CarvedRockRepository : ICarvedRockRepository
     {
         try
         {
-            _context.Update(product);
-            await _context.SaveChangesAsync();
+            context.Update(product);
+            await context.SaveChangesAsync();
         }
         catch (DbUpdateConcurrencyException)
         {
-            if (_context.Products.Any(e => e.Id == product.Id))
+            if (context.Products.Any(e => e.Id == product.Id))
             {
                 // product exists and update exception is real
                 throw;
@@ -58,22 +52,21 @@ public class CarvedRockRepository : ICarvedRockRepository
             throw new Exception("Simulated exception trying to remove product!");
         }
 
-        var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == productIdToRemove);
+        var product = await context.Products.FirstOrDefaultAsync(p => p.Id == productIdToRemove);
         if (product != null)
         {
-            _context.Products.Remove(product);
-            await _context.SaveChangesAsync();
+            context.Products.Remove(product);
+            await context.SaveChangesAsync();
         }
     }
 
     public async Task<Category?> GetCategoryByIdAsync(int categoryId)
     {
-        return await _context.Categories.FirstOrDefaultAsync(m => m.Id == categoryId);
+        return await context.Categories.FirstOrDefaultAsync(m => m.Id == categoryId);
     }
 
     public async Task<List<Category>> GetAllCategoriesAsync()
     {
-        return await _context.Categories.ToListAsync();
+        return await context.Categories.ToListAsync();
     }
 }
-
